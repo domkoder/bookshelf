48207f5b1fd72b32e841371942a45f2626088977
Nandom <nandommamdam@gmail.com>
2021-07-15 04:24:03 +0100

    exercise 01 extra 1 Use `@reach/dialog`

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none')
+
   return (
     <div>
       <Logo width={80} height={80} />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login clicked')}>Loin</button>
+        <button onClick={() => setOpenModal('login')}>Loin</button>
       </div>
       <div>
-        <button onClick={() => alert('register clicked')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>clos</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>clos</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </div>
   )
 }
